d73c0dba779bc47eec0e134cd6a8ee4e3dc3726b
trulite <satyaprakash007@gmail.com>
2015-05-27 06:25:24 +0530

    Fix for https://github.com/tuplejump/ddf-with-flink/issues/30

--- flink/src/test/scala/io/flink/ddf/FlinkDDFManagerSpec.scala
+++ flink/src/test/scala/io/flink/ddf/FlinkDDFManagerSpec.scala
@@ -3,14 +3,15 @@ package io.flink.ddf
 import io.ddf.DDFManager
 import org.scalatest.{FlatSpec, Matchers}
 
-class FlinkDDFManagerSpec extends FlatSpec with Matchers {
+class FlinkDDFManagerSpec extends BaseSpec with Matchers {
 
   it should "load data from file" in {
-    val flinkDDFManager = DDFManager.get("flink")
-    val ddf = flinkDDFManager.loadTable(getClass.getResource("/airline.csv").getPath, ",")
     ddf.getNamespace should be("FlinkDDF")
     ddf.getColumnNames should have size (29)
 
+  }
+
+  it should "be addressable via URI" in {
     ddf.getUri should be("ddf://" + ddf.getNamespace + "/" + ddf.getName)
     flinkDDFManager.getDDF("ddf://" + ddf.getNamespace + "/" + ddf.getName) should be(ddf)
   }
