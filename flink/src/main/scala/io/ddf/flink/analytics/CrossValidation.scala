05bc31c11bb574e9a7437b565e71094b21161ab9
Rohit Rai <rohit@tuplejump.com>
2015-07-16 10:51:11 +0530

    Cross validation and confusion matrix implementation for flink

---
+++ flink/src/main/scala/io/ddf/flink/analytics/CrossValidation.scala
@@ -0,0 +1,144 @@
+package io.ddf.flink.analytics
+
+import io.ddf.DDF
+import io.ddf.DDFManager
+import io.ddf.content.Schema
+import io.ddf.exception.DDFException
+import io.ddf.flink.FlinkDDF
+import java.util.Random
+import io.ddf.{DDFManager, DDF}
+import java.util.{List => JList}
+import io.ddf.exception.DDFException
+import java.util
+import io.ddf.content.Schema
+import org.apache.flink.api.common.functions.RichFilterFunction
+import org.apache.flink.api.scala.DataSet
+import org.apache.flink.api.table.Row
+
+class RandomFilterFunction[T](seed: Long, lower: Double, upper: Double, isTraining: Boolean) extends RichFilterFunction[T] {
+  val rand = new Random(seed)
+  override def filter(t: T): Boolean = {
+    if(isTraining){
+      val z = rand.nextDouble
+      z < lower || z >= upper
+    } else {
+      val z = rand.nextDouble
+      z < lower || z >= upper
+    }
+  }
+}
+
+object CrossValidation {
+  /** Return an Iterator of size k of (train, test) RDD Tuple
+    * for which the probability of each element belonging to each split is (trainingSize, 1-trainingSize).
+    * The train & test data across k split are shuffled differently (different random seed for each iteration).
+    */
+  def randomSplit[T](dataset: DataSet[T], numSplits: Int, trainingSize: Double, seed: Long)(implicit _cm: ClassManifest[T]): Iterator[(DataSet[T], DataSet[T])] = {
+    require(0 < trainingSize && trainingSize < 1)
+    val rg = new Random(seed)
+    (1 to numSplits).map(_ => rg.nextInt).map(z =>
+      (dataset.filter(new RandomFilterFunction[T](z, 0, 1.0 - trainingSize, true)),
+        dataset.filter(new RandomFilterFunction[T](z, 0, 1.0 - trainingSize, false)))).toIterator
+  }
+
+  /** Return an Iterator of of size k of (train, test) RDD Tuple
+    * for which the probability of each element belonging to either split is ((k-1)/k, 1/k).
+    * The location of the test data is shifted consistently between folds
+    * so that the resulting test sets are pair-wise disjoint.
+    */
+  def kFoldSplit[T](dataset: DataSet[T], numSplits: Int, seed: Long)(implicit _cm: ClassManifest[T]): Iterator[(DataSet[T], DataSet[T])] = {
+    require(numSplits > 0)
+    (for (lower <- 0.0 until 1.0 by 1.0 / numSplits)
+      yield (dataset.filter(new RandomFilterFunction[T](seed, lower, lower + 1.0 / numSplits, true)),
+        dataset.filter(new RandomFilterFunction[T](seed, lower, lower + 1.0 / numSplits, false)))
+      ).toIterator
+  }
+
+  def DDFRandomSplit(ddf: DDF, numSplits: Int, trainingSize: Double, seed: Long): JList[JList[DDF]] = {
+    var unitType: Class[_] = null
+    var splits: Iterator[(DataSet[_], DataSet[_])] = null
+    if (ddf.getRepresentationHandler.has(classOf[DataSet[_]], classOf[Row])) {
+      val rdd = ddf.getRepresentationHandler().get(classOf[DataSet[_]], classOf[Row]).asInstanceOf[DataSet[Row]]
+      splits = randomSplit(rdd, numSplits, trainingSize, seed)
+      unitType = classOf[Row]
+
+    } else if (ddf.getRepresentationHandler.has(classOf[DataSet[_]], classOf[Array[Double]])) {
+      val rdd = ddf.getRepresentationHandler().get(classOf[DataSet[_]], classOf[Array[Double]]).asInstanceOf[DataSet[Array[Double]]]
+      splits = randomSplit(rdd, numSplits, trainingSize, seed)
+      unitType = classOf[Array[Double]]
+
+    } else if (ddf.getRepresentationHandler.has(classOf[DataSet[_]], classOf[Array[Object]])) {
+      val rdd = ddf.getRepresentationHandler().get(classOf[DataSet[_]], classOf[Array[Object]]).asInstanceOf[DataSet[Array[Object]]]
+      splits = randomSplit(rdd, numSplits, trainingSize, seed)
+      unitType = classOf[Array[Object]]
+
+    } else {
+      val rdd = ddf.getRepresentationHandler().get(classOf[DataSet[_]], classOf[Array[Object]]).asInstanceOf[DataSet[Array[Object]]]
+      if (rdd == null) throw new DDFException("Cannot get RDD of Representation Array[Double], Array[Object] or Row")
+      splits = randomSplit(rdd, numSplits, trainingSize, seed)
+      unitType = classOf[Array[Object]]
+    }
+    if (splits == null) {
+      throw new DDFException("Error getting cross validation for DDF")
+    }
+    getDDFCVSetsFromDataSets(splits, ddf.getManager, ddf.getSchema, ddf.getNamespace, unitType)
+  }
+
+  def DDFKFoldSplit(ddf: DDF, numSplits: Int, seed: Long): JList[JList[DDF]] = {
+    var unitType: Class[_] = null
+    var splits: Iterator[(DataSet[_], DataSet[_])] = null
+
+    if (ddf.getRepresentationHandler.has(classOf[DataSet[_]], classOf[Row])) {
+      val rdd = ddf.getRepresentationHandler().get(classOf[DataSet[_]], classOf[Row]).asInstanceOf[DataSet[Row]]
+      splits = kFoldSplit(rdd, numSplits, seed)
+      unitType = classOf[Row]
+
+    } else if (ddf.getRepresentationHandler.has(Array(classOf[DataSet[_]], classOf[Array[Double]]): _*)) {
+      val rdd = ddf.getRepresentationHandler().get(classOf[DataSet[_]], classOf[Array[Double]]).asInstanceOf[DataSet[Array[Double]]]
+      splits = kFoldSplit(rdd, numSplits, seed)
+      unitType = classOf[Array[Double]]
+
+    } else if (ddf.getRepresentationHandler.has(Array(classOf[DataSet[_]], classOf[Array[Object]]): _*)) {
+      val rdd = ddf.getRepresentationHandler().get(classOf[DataSet[_]], classOf[Array[Object]]).asInstanceOf[DataSet[Array[Object]]]
+      splits = kFoldSplit(rdd, numSplits, seed)
+      unitType = classOf[Array[Object]]
+
+    } else {
+      val rdd = ddf.getRepresentationHandler().get(classOf[DataSet[_]], classOf[Array[Object]]).asInstanceOf[DataSet[Array[Object]]]
+      if (rdd == null) throw new DDFException("Cannot get RDD of representation Array[Double], Array[Object] or Row")
+      splits = kFoldSplit(rdd, numSplits, seed)
+      unitType = classOf[Array[Object]]
+    }
+
+    if (splits == null) {
+      throw new DDFException("Error getting cross validation for DDF")
+    }
+    getDDFCVSetsFromDataSets(splits, ddf.getManager, ddf.getSchema, ddf.getNamespace, unitType)
+  }
+
+  /**
+   * Get set of Cross Validation of DDFs from RDD splits
+   * @param splits Iterator of tuple of (train, test) RDD
+   * @param unitType unitType of returned DDF
+   * @return List of Cross Validation sets
+   */
+  private def getDDFCVSetsFromDataSets(splits: Iterator[(DataSet[_], DataSet[_])], manager: DDFManager, schema: Schema, nameSpace: String, unitType: Class[_]): JList[JList[DDF]] = {
+    val cvSets: JList[JList[DDF]] = new util.ArrayList[JList[DDF]]()
+
+    for ((train, test) <- splits) {
+      val aSet = new util.ArrayList[DDF]()
+
+      val trainSchema = new Schema(null, schema.getColumns)
+
+      val trainDDF = manager.newDDF(manager, train, Array(classOf[DataSet[_]], unitType), nameSpace, null, trainSchema)
+
+      val testSchema = new Schema(null, schema.getColumns)
+      val testDDF = manager.newDDF(manager, test, Array(classOf[DataSet[_]], unitType), nameSpace, null, testSchema)
+
+      aSet.add(trainDDF)
+      aSet.add(testDDF)
+      cvSets.add(aSet)
+    }
+    return cvSets
+  }
+}
--- flink/src/main/scala/io/ddf/flink/ml/FlinkMLSupporter.scala
+++ flink/src/main/scala/io/ddf/flink/ml/FlinkMLSupporter.scala
@@ -5,8 +5,11 @@ import java.{lang, util}
 import io.ddf.DDF
 import io.ddf.content.Schema
 import io.ddf.content.Schema.Column
+import io.ddf.flink.FlinkDDF
+import io.ddf.flink.analytics.CrossValidation
 import io.ddf.misc.{Config, ADDFFunctionalGroupHandler}
 import io.ddf.ml.{IModel, ISupportML, MLSupporter => CoreMLSupporter, Model}
+import org.apache.flink.api.common.functions.{ReduceFunction, MapFunction}
 import org.apache.flink.api.scala.DataSet
 import org.apache.flink.ml.common.ParameterMap
 import org.apache.flink.ml.pipeline.{Estimator, Predictor}
@@ -51,12 +54,43 @@ class FlinkMLSupporter(ddf: DDF) extends ADDFFunctionalGroupHandler(ddf) with IS
     this.getManager.newDDF(this.getManager, resultDs, (classOf[DataSet[_]] +: resultType).toArray, this.getManager.getNamespace, null, schema)
   }
 
-  override def CVRandom(k: Int, trainingSize: Double, seed: lang.Long): util.List[util.List[DDF]] = ???
+  override def CVRandom(k: Int, trainingSize: Double, seed: lang.Long): util.List[util.List[DDF]] = {
+    CrossValidation.DDFRandomSplit(this.getDDF, k, trainingSize, seed)
+  }
 
-  override def CVKFold(k: Int, seed: lang.Long): util.List[util.List[DDF]] = ???
+  override def CVKFold(k: Int, seed: lang.Long): util.List[util.List[DDF]] = {
+    CrossValidation.DDFKFoldSplit(this.getDDF, k, seed)
+  }
 
-  override def getConfusionMatrix(model: IModel, threshold: Double): Array[Array[Long]] = ???
+  override def getConfusionMatrix(model: IModel, threshold: Double): Array[Array[Long]] = {
+    val ddf: FlinkDDF = this.getDDF.asInstanceOf[FlinkDDF]
+    val predictions: FlinkDDF = ddf.ML.applyModel(model, true, false).asInstanceOf[FlinkDDF]
+
+    // Now get the underlying RDD to compute
+    val yTrueYPred: DataSet[Array[Double]] = predictions.getRepresentationHandler.get(classOf[DataSet[_]], classOf[Array[Double]]).asInstanceOf[DataSet[Array[Double]]]
+    val threshold1: Double = threshold
+    val cm: Array[Long] = yTrueYPred.map(new MapFunction[Array[Double], Array[Long]]() {
+      override def map(params: Array[Double]): Array[Long] = {
+        val isPos: Byte = toByte(params(0) > threshold1)
+        val predPos: Byte = toByte(params(1) > threshold1)
+        val result: Array[Long] = Array[Long](0L, 0L, 0L, 0L)
+        result(isPos << 1 | predPos) = 1L
+        result
+      }
+    }).reduce(new ReduceFunction[Array[Long], Array[Long], Array[Long]]() {
+      override def reduce(a: Array[Long], b: Array[Long]): Array[Long] = {
+        Array[Long](a(0) + b(0), a(1) + b(1), a(2) + b(2), a(3) + b(3))
+      }
+    }).collect().flatten.toArray
+
+    Array[Array[Long]](Array[Long](cm(3), cm(2)), Array[Long](cm(1), cm(0)))
+  }
+
+  private def toByte(exp: Boolean): Byte = {
+    if (exp) 1 else 0
+  }
 }
+
 
 object DatasetTypes {
 
